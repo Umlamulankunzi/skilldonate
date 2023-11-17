da28110d679a0c2f8bd249134a192e723004071b
Prince D Jele <Umlamulankunzi@gmail.com>
2023-11-17 21:36:35 +0200

    connected all urls config of paths to main root url config
    added generic views which return text to enable me to check if the urls to each app is working

--- skilldonate/skilldonate/settings.py
+++ skilldonate/skilldonate/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'charities',
     'volunteers',
-    'volunteers',
+    'skills',
 ]
 
 MIDDLEWARE = [
--- skilldonate/skilldonate/views.py
+++ skilldonate/skilldonate/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def charities(request):
-    return HttpResponse("<h1>CHARITIES</h1>")
+def index(request):
+    return HttpResponse("<h1>HOME PAGE</h1>")
