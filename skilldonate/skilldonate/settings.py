8d22d82be0e75a0e158730ac79b37efc5246eb57
Prince D Jele <Umlamulankunzi@gmail.com>
2023-11-17 20:44:32 +0200

    updated settings.py and requirements.txt
    switched databse to use MySQL in seetings.py and updated requirements.txt to include mysqlclient for connecting to MySQL DB

--- skilldonate/skilldonate/settings.py
+++ skilldonate/skilldonate/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'charities',
+    'volunteers',
+    'volunteers',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +77,20 @@ WSGI_APPLICATION = 'skilldonate.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'old_default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'skilldonate',
+        'USER': 'umlamulankunzi',
+        'PASSWORD': 'umla_dev_578',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
+
 }
 
 
